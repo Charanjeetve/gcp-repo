1f957825655719996e607f536149629a19890924
Charanjeetve <118544894+Charanjeetve@users.noreply.github.com>
2023-02-13 21:01:05 +0530

    vpc veriable

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+    type = string
+    default = "project-101-377013"
+}
+variable "region" {
+    type = string
+    default = "europe-north1"
+}
+variable "vpc_name" {
+    type = string
+    default = "gcp-lab-01"
+}
+
+
